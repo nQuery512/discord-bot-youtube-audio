f7cbe88d258d0097cf3a3bf0c199bd45e0f7767c
nQuery512 <nQuery512@gmail.com>
2019-06-24 00:16:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var regex_str = "(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9
 var regex_url = new RegExp(regex_str);
 var opts = {
   maxResults: 5,
-  key: 'AIzaSyA33qetqZNY0N3HRGDRkBNzwJ9aZpbDIy8'
+  key: YOUTUBE_API_KEY
 };
 
 // Lancement du serveur
@@ -133,5 +133,5 @@ bot.on('message', message => {
 	  	}
 	}
 });
-var TOKEN = "NTkxOTUxMTU2MTg4NTQ1MDI1.XQ4VYg.P_GpCxZ9RM-agjHAw2iU0WhPzbM";
+var TOKEN = DISCORD_BOT_API_KEY;
 bot.login(TOKEN);
