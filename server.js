fa6cdc74ff6a59a7827b8884fe980271ba86a873
nQuery512 <nQuery512@gmail.com>
2019-06-23 23:58:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,105 +8,130 @@ var regex_str = "(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9
 var regex_url = new RegExp(regex_str);
 var opts = {
   maxResults: 5,
-  key: '##################################'
+  key: 'AIzaSyA33qetqZNY0N3HRGDRkBNzwJ9aZpbDIy8'
 };
 
-bot.on('ready', function () {
+// Lancement du serveur
+bot.on('ready',() => {
   console.log("Je suis connecté !")
 })
 
-bot.on('message', message => {
-	console.log(message.content)
-  	if (message.content.toLowerCase() === 'ping') {
-    	message.channel.send("pong", {tts:true})
-	}
-
- 	if (message.content.startsWith('!play')) {
-
-	    // On récupère le premier channel audio du serveur
-	    let voiceChannel = message.guild.channels
-	      .filter(function (channel) { return channel.name === "La Taverne"})
-	      .first()
-	    console.log("channel: "+voiceChannel);
-
-	    // On récupère les arguments de la commande 
-	    // il faudrait utiliser une expression régulière pour valider le lien youtube
-	    let args = message.content.split(' ')
-
-	    if(args[1].trim().match(regex_url))
-	    {
-
-		    // On rejoint le channel audio
-		    voiceChannel
-		      .join()
-		      .then(function (connection) {
-		        // On démarre un stream à partir de la vidéo youtube
-		        let stream = YoutubeStream(args[1])
-		        stream.on('error', function () {
-		        	console.log("erreur");
-		          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-		          connection.disconnect()
-		          process.exit(0);
-		        })
-		        // On envoie le stream au channel audio
-		        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
-		        connection
-		          .playStream(stream)
-		          .on('end', function () {
-		            connection.disconnect()
-		        })
-	    	});
-	    }
-	    else
-		{
-			console.log('youtube search: '+ args[1].trim());
-			search(args[1].trim(), opts, function(err, results) {
-				if(err) return console.log(err);
-				//console.dir(results[0]);
-				for(result of results)
-				{
-					console.log(result.kind)
-					if(result.kind == 'youtube#video')			
-					{
-						
-				 		console.dir(result.link);
-				  		// On rejoint le channel audio
-					    voiceChannel
-					    	.join()
-						    .then(function (connection) {
-					        // On démarre un stream à partir de la vidéo youtube
-						    let stream = YoutubeStream(result.link)
-						    stream.on('error', function () {
-						    	console.log("erreur");
-						        message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-						        connection.disconnect()
-						        process.exit(0);
-						    })
-						    message.reply("Vous écoutez "+decodeURIComponent(result.title)+' '+result.thumbnails.default.url);
-						    // On envoie le stream au channel audio
-						    // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
-						    connection
-						    	.playStream(stream)
-						        .on('end', function () {
-						        connection.disconnect()
-						    })
-					    });
-			  			break;
-					}
-				}
-			});
-			
-		}
-  	}
-  	if (message.content.startsWith('!stop'))
-  	{
-  		let voiceChannel = message.guild.channels
-	      .filter(function (channel) { return channel.name === "La Taverne"})
-	      .first()
-
-	    voiceChannel.leave();
-  	}
-
+// Gestion des nouveau arrivants
+bot.on("guildMemberAdd", (member) =>
+{
+	//console.log(member);
+	console.log("\n"+member.user.username);
+	member.send("Bienvenue "+ member.user.username+" sur le Discord de la guilde The Iron Flesh !!\n Si tu souhaite nous rejoindre je t'invite à prendre connaissance du contenu des channels #Règle et #Recrutement");
 });
-var TOKEN = "######################################"";
+
+// Gestion des messages
+bot.on('message', message => {
+	console.log(message.content);
+
+	if(message.channel.name == "recrutement")
+	{
+		console.log(message.member.user);
+
+		process.exit(0);
+	} 
+
+	if(message.channel.name == "commande-bot")
+	{
+	
+	  	if (message.content.toLowerCase() === 'ping') {
+	    	message.channel.send("pong", {tts:true})
+		}
+
+	 	if (message.content.startsWith('!play')) {
+
+
+		    // On récupère le channel audio du serveur
+		   	console.log(message.member.user.lastMessage.member.voiceChannelID);
+		   	//process.exit(0);
+		    let voiceChannel = message.guild.channels
+		      .filter(function (channel) { return channel.id === message.member.user.lastMessage.member.voiceChannelID})
+		      .first()
+		    console.log("channel: "+voiceChannel);
+
+		    // On récupère les arguments de la commande 
+		    // il faudrait utiliser une expression régulière pour valider le lien youtube
+		    let args = message.content.split(' ')
+
+		    if(args[1].trim().match(regex_url))
+		    {
+
+			    // On rejoint le channel audio
+			    voiceChannel
+			      .join()
+			      .then(function (connection) {
+			        // On démarre un stream à partir de la vidéo youtube
+			        let stream = YoutubeStream(args[1])
+			        stream.on('error', function () {
+			        	console.log("erreur");
+			          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+			          connection.disconnect()
+			          
+			        })
+			        // On envoie le stream au channel audio
+			        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
+			        connection
+			          .playStream(stream)
+			          .on('end', function () {
+			            connection.disconnect()
+			        })
+		    	});
+		    }
+		    else
+			{
+				console.log('youtube search: '+ args[1].trim());
+				search(args[1].trim(), opts, function(err, results) {
+					if(err) return console.log(err);
+					//console.dir(results[0]);
+					for(result of results)
+					{
+						console.log(result.kind)
+						// On elimine les résultats comme les pubs, les chaines etc...
+						if(result.kind == 'youtube#video')			
+						{
+							
+					 		console.dir(result.link);
+					  		// On rejoint le channel audio
+						    voiceChannel
+						    	.join()
+							    .then(function (connection) {
+						        // On démarre un stream à partir de la vidéo youtube
+							    let stream = YoutubeStream(result.link)
+							    stream.on('error', function () {
+							    	console.log("erreur");
+							        message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+							        connection.disconnect()
+							        
+							    })
+							    message.reply("Vous écoutez "+decodeURIComponent(result.title)+' '+result.thumbnails.default.url);
+							    // On envoie le stream au channel audio
+							    // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
+							    connection
+							    	.playStream(stream)
+							        .on('end', function () {
+							        connection.disconnect()
+							    })
+						    });
+				  			break;
+						}
+					}
+				});
+				
+			}
+	  	}
+	  	if (message.content.startsWith('!stop'))
+	  	{
+	  		let voiceChannel = message.guild.channels
+		      .filter(function (channel) { return channel.id === message.member.user.lastMessage.member.voiceChannelID})
+		      .first()
+
+		    voiceChannel.leave();
+	  	}
+	}
+});
+var TOKEN = "NTkxOTUxMTU2MTg4NTQ1MDI1.XQ4VYg.P_GpCxZ9RM-agjHAw2iU0WhPzbM";
 bot.login(TOKEN);
